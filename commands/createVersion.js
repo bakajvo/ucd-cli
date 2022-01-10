b5cae3c38fcdff9b0621cca30f3c8c719fe6df58
Vojtěch <vojtech@bakaj.cz>
2022-01-10 14:13:30 +0100

    Add axios dependency ➕

--- commands/createVersion.js
+++ commands/createVersion.js
@@ -1,6 +1,7 @@
 import Log from "../utils/log.js";
 import chalk from 'chalk';
 import isEmpty from "lodash/isEmpty.js";
+import axios from 'axios';
 
 const createVersion = ({debug, component, componentVersion}) => {
     if(isEmpty(component)) {
